20bcfcf2189ef8210a984ffa1b8d7aa1109803e4
encetamasb <encetamasb@gmail.com>
2019-12-09 06:50:06 +0100

    Added Op9 and RelativeMode

--- d09.go
+++ d09.go
@@ -23,27 +23,30 @@ type Mode int
 const (
 	PositionMode  Mode = 0
 	ImmediateMode Mode = 1
+	RelativeMode  Mode = 2
 )
 
 type Instr int
 
 const (
-	Add      Instr = 1
-	Mul      Instr = 2
-	In       Instr = 3
-	Out      Instr = 4
-	JmpTrue  Instr = 5
-	JmpFalse Instr = 6
-	LessThan Instr = 7
-	Equals   Instr = 8
-	Halt     Instr = 99
+	Add        Instr = 1
+	Mul        Instr = 2
+	In         Instr = 3
+	Out        Instr = 4
+	JmpTrue    Instr = 5
+	JmpFalse   Instr = 6
+	LessThan   Instr = 7
+	Equals     Instr = 8
+	AdjRelBase Instr = 9
+	Halt       Instr = 99
 )
 
 type IntProg []int
 
 type VM struct {
-	p     IntProg
+	prog  IntProg
 	pos   Position
+	rbase Position
 	state State
 	in    []int
 	out   []int
@@ -69,34 +72,53 @@ func (p IntProg) Clone() IntProg {
 	return newp
 }
 
-func (p IntProg) At(pos Position, mode Mode) int {
+func (vm VM) Read(pos Position, mode Mode) int {
+	prog := vm.prog
 	switch mode {
 	case PositionMode:
-		return p[p[pos]]
+		return prog[prog[pos]]
 	case ImmediateMode:
-		return p[pos]
+		return prog[pos]
+	case RelativeMode:
+		return prog[vm.rbase+pos]
+	}
+	panic("ops")
+}
+
+func (vm VM) Write(pos Position, mode Mode, v int) {
+	prog := vm.prog
+	fmt.Println(pos, mode, v)
+	switch mode {
+	case PositionMode:
+		prog[prog[pos]] = v
+		return
+	//case ImmediateMode:
+	//	panic("illegal")
+	//	prog[pos] = v
+	case RelativeMode:
+		prog[vm.rbase+pos] = v
+		return
 	}
 	panic("ops")
 }
 
 func (vm VM) step() VM {
-	p := vm.p
 	pos := vm.pos
-	cur := p[pos]
+	cur := vm.prog[pos]
 	op := extractOp(cur)
 
-	//fmt.Println(op)
+	fmt.Println(op, vm.prog[pos:pos+4])
 	switch op.DE {
 	case Add:
-		a := p.At(pos+1, op.A)
-		b := p.At(pos+2, op.B)
-		p[p[pos+3]] = a + b
+		a := vm.Read(pos+1, op.A)
+		b := vm.Read(pos+2, op.B)
+		vm.Write(pos+3, op.C, a+b)
 		vm.pos += 4
 		return vm
 	case Mul:
-		a := p.At(pos+1, op.A)
-		b := p.At(pos+2, op.B)
-		p[p[pos+3]] = a * b
+		a := vm.Read(pos+1, op.A)
+		b := vm.Read(pos+2, op.B)
+		vm.Write(pos+3, op.C, a*b)
 		vm.pos += 4
 		return vm
 	case In:
@@ -113,52 +135,57 @@ func (vm VM) step() VM {
 			}
 			n = n2
 		}
-		p[p[pos+1]] = n
+		vm.Write(pos+1, op.A, n)
 		vm.pos += 2
 		return vm
 	case Out:
-		v := p.At(pos+1, op.A)
+		v := vm.Read(pos+1, op.A)
 		//fmt.Print(v)
 		vm.out = append(vm.out, v)
 		vm.pos += 2
 		return vm
 	case JmpTrue:
-		v := p.At(pos+1, op.A)
+		v := vm.Read(pos+1, op.A)
 		if v != 0 {
 			vm.pos = Position(
-				p.At(pos+2, op.B))
+				vm.Read(pos+2, op.B))
 			return vm
 		}
 		vm.pos += 3
 		return vm
 	case JmpFalse:
-		v := p.At(pos+1, op.A)
+		v := vm.Read(pos+1, op.A)
 		if v == 0 {
 			vm.pos = Position(
-				p.At(pos+2, op.B))
+				vm.Read(pos+2, op.B))
 			return vm
 		}
 		vm.pos += 3
 		return vm
 	case LessThan:
-		a := p.At(pos+1, op.A)
-		b := p.At(pos+2, op.B)
+		a := vm.Read(pos+1, op.A)
+		b := vm.Read(pos+2, op.B)
 		if a < b {
-			p[p[pos+3]] = 1
+			vm.Write(pos+3, op.C, 1)
 		} else {
-			p[p[pos+3]] = 0
+			vm.Write(pos+3, op.C, 0)
 		}
 		vm.pos += 4
 		return vm
 	case Equals:
-		a := p.At(pos+1, op.A)
-		b := p.At(pos+2, op.B)
+		a := vm.Read(pos+1, op.A)
+		b := vm.Read(pos+2, op.B)
 		if a == b {
-			p[p[pos+3]] = 1
+			vm.Write(pos+3, op.C, 1)
 		} else {
-			p[p[pos+3]] = 0
+			vm.Write(pos+3, op.C, 0)
 		}
 		vm.pos += 4
+		return vm
+	case AdjRelBase:
+		a := vm.Read(pos+1, op.A)
+		vm.rbase += Position(a)
+		vm.pos += 2
 		return vm
 	case Halt:
 		vm.pos += 1
@@ -228,7 +255,7 @@ func main() {
 			in := make([]int, 0)
 			in = append(in, phase)
 			in = append(in, signal)
-			vm := VM{prog.Clone(), Position(0), Running, in, make([]int, 0)}
+			vm := VM{prog.Clone(), Position(0), Position(0), Running, in, make([]int, 0)}
 
 			vm = vm.run()
 
@@ -241,47 +268,4 @@ func main() {
 	}
 
 	fmt.Println("Result1:", maxSignal)
-
-	perms = getPerms([5]int{0, 1, 2, 3, 4}, 0)
-	maxSignal = 0
-	for _, perm := range perms {
-		signal := 0
-
-		vms := make([]VM, 0, 5)
-		for i := 0; i < 5; i++ {
-			in := make([]int, 0)
-			in = append(in, perm[i]+5) // phase
-			out := make([]int, 0)
-			vms = append(vms, VM{prog.Clone(), Position(0), Running, in, out})
-		}
-
-		vms[0].in = append(vms[0].in, signal) //input signal
-
-		i := 0
-		for {
-			vm := vms[i]
-			for vm.state == Running && len(vm.out) < 1 {
-				vm = vm.step()
-			}
-			vms[i] = vm
-
-			if vm.state == Halted {
-				signal = vms[4].out[0]
-				break
-			} else if vm.state == Error {
-				panic("ops")
-			}
-
-			j := (i + 1) % 5
-			vms[j].in = append(vms[j].in, vm.out[len(vm.out)-1])
-			vms[j].out = make([]int, 0)
-			i = j
-		}
-
-		if signal > maxSignal {
-			maxSignal = signal
-		}
-	}
-
-	fmt.Println("Result2:", maxSignal)
 }
