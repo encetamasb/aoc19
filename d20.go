b66f75a9d827e3fb11c9f8fc4a8effeb430882b4
encetamasb <encetamasb@gmail.com>
2019-12-21 07:53:27 +0100

    Shortest path found

--- d20.go
+++ d20.go
@@ -90,6 +90,27 @@ func (m Map) Show() {
 	fmt.Println()
 }
 
+func (m Map) ShowPath(path []Pair) {
+	visited := make(map[Pair]bool)
+	for i := 0; i < len(path); i++ {
+		visited[path[i]] = true
+	}
+	fmt.Println()
+	for j := 0; j < m.H; j++ {
+		for i := 0; i < m.W; i++ {
+			pos := Pair{i, j}
+			_, exists := visited[pos]
+			if exists {
+				fmt.Print("@")
+			} else {
+				fmt.Print(m.At(pos, Nothing))
+			}
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
+
 func LoadMap(path string) Map {
 	raw, _ := ioutil.ReadFile(path)
 	lines := strings.Split(strings.Trim(string(raw), "\n"), "\n")
@@ -143,9 +164,85 @@ func LoadMap(path string) Map {
 	return Map{w, h, fields, portals, labels}
 }
 
+func (m Map) LabelToPos(label string) (Pair, bool) {
+	for pos, s := range m.Labels {
+		if label == s {
+			return pos, true
+		}
+	}
+	return Pair{}, false
+}
+
+func (m Map) MinPath(from, to string) ([]Pair, int) {
+	start, ok := m.LabelToPos(from)
+	if !ok {
+		panic("ops")
+	}
+	end, ok := m.LabelToPos(to)
+	if !ok {
+		panic("ops")
+	}
+	fmt.Println(start, end)
+	path := make([]Pair, 0)
+
+	type Rec struct {
+		p    Pair
+		dist int
+		from *Rec
+	}
+
+	visited := make(map[Pair]*Rec)
+
+	q := make([]*Rec, 0)
+	q = append(q, &Rec{start, 0, nil})
+
+	for len(q) > 0 {
+		rec := q[0]
+		q = q[1:]
+
+		prev, exists := visited[rec.p]
+		if exists {
+			if prev.dist > rec.dist {
+				visited[rec.p] = rec
+			} else {
+				continue
+			}
+		} else {
+			visited[rec.p] = rec
+		}
+
+		for i := 0; i < 4; i++ {
+			nextp := rec.p.NextTo(Dir(i))
+			f := m.At(nextp, Wall)
+			if f == Empty {
+				q = append(q, &Rec{nextp, rec.dist + 1, rec})
+			} else if f.IsLabel() && !m.At(rec.p, Nothing).IsLabel() {
+				nextp, exists := m.Portals[rec.p]
+				if exists {
+					q = append(q, &Rec{nextp, rec.dist + 1, rec})
+				}
+			}
+
+		}
+	}
+
+	cur := visited[end]
+	for cur != nil {
+		path = append(path, cur.p)
+		cur = cur.from
+	}
+
+	return path, visited[end].dist
+}
+
 func main() {
 	m := LoadMap(os.Args[1])
 	m.Show()
+
 	fmt.Println(m.Labels)
 	fmt.Println(m.Portals)
+
+	path, min := m.MinPath("AA", "ZZ")
+	m.ShowPath(path)
+	fmt.Println("Result1:", min)
 }
